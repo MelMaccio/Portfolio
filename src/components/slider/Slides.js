1df64c16457f32558855c43c681cdbaab2ac6e1c
Melina Maccio <melina.maccio416@gmail.com>
2022-09-23 12:01:19 -0300

    feat: add projects

--- src/components/slider/Slides.js
+++ src/components/slider/Slides.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "./Slider.css";
+import pm from "../../media/pm.png";
+import pri from "../../media/PRI.jpg";
 
 const slidesInfo = [
   {
-    src: "https://cdn.pixabay.com/photo/2016/12/19/08/39/mobile-phone-1917737_960_720.jpg",
-    alt: "Project 1",
-    desc: "Project 1",
+    src: pm,
+    alt: "Prode Master",
+    desc: "Prode Master",
+    url: "https://prodemaster.netlify.app/",
   },
   {
-    src: "https://cdn.pixabay.com/photo/2017/10/31/19/05/web-design-2906159_960_720.jpg",
-    alt: "Project 2",
-    desc: "Project 2",
+    src: pri,
+    alt: "Countries App",
+    desc: "Countries App",
+    url: "https://github.com/MelMaccio/PI-Countries",
   },
   {
     src: "https://cdn.pixabay.com/photo/2015/02/05/08/06/macbook-624707_960_720.jpg",
@@ -21,10 +25,12 @@ const slidesInfo = [
 
 const slides = slidesInfo.map((slide) => (
   <div className="slide-container">
-    <img src={slide.src} alt={slide.alt} />
-    <div className="slide-desc">
-      <span>{slide.desc}</span>
-    </div>
+    <a href={slide.url} target="_blank" rel="noreferrer">
+      <img src={slide.src} alt={slide.alt} />
+      <div className="slide-desc">
+        <span>{slide.desc}</span>
+      </div>
+    </a>
   </div>
 ));
 
